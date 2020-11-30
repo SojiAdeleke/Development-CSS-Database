177f8c290f32a1b9a310b26b6eba640fb185da56
isaiahduncan <isaiahduncan1212@yahoo.com>
2020-11-30 13:24:33 -0500

    making new method to check iap for each slide rather than for each scholar

--- IAPCheck.ts
+++ IAPCheck.ts
@@ -19,12 +19,12 @@ function statusToString(iapStatus: IAP_STATUS): string {
 function updateIAP(): string[][] { // 
     let res = new Array(scholarInfo.length) as string[][]; 
     let date = new Date();
-    let toy = (date.getMonth() >= 7 && date.getMonth() <= 11 ? TIMEOFYEAR.FALL : TIMEOFYEAR.SPRING)
+    let timeOfYear = (date.getMonth() >= 7 && date.getMonth() <= 11 ? TIMEOFYEAR.FALL : TIMEOFYEAR.SPRING)
     
     for (let i = 0; i < scholarInfo.length; i++) {
         let scholar = scholarInfo[i];
         if(scholar.iapStatus == IAP_STATUS.INCOMPLETE){            
-            const newstatus = checkIAP(scholar.cohort, scholar.lastName+" "+scholar.lastName, toy);
+            const newstatus = checkIAP(scholar.cohort, scholar.firstName+" "+scholar.lastName, timeOfYear);
             res[i] = new Array(statusToString(newstatus));
         } else {
             res[i] = new Array(statusToString(scholar.iapStatus));
@@ -33,6 +33,27 @@ function updateIAP(): string[][] { //
 
     return res;
     
+}
+
+function updateIAPBySlides(): string[][] {
+    let statusArr = new Array(scholarInfo.length) as IAP_STATUS[];
+    for(let i = 0; i < scholarInfo.length; i++){
+        statusArr[i] = scholarInfo[i].iapStatus;
+    }
+
+    let res = new Array(scholarInfo.length) as string[][]; 
+    let date = new Date();
+    let timeOfYear = (date.getMonth() >= 7 && date.getMonth() <= 11 ? TIMEOFYEAR.FALL : TIMEOFYEAR.SPRING);
+    var schoolYear = startOfSemester.year;
+
+    for(let year = schoolYear; year > year-4; year--){
+        checkIAPBySlides(year, timeOfYear, statusArr);//updating statusArr
+    }
+
+    for(let i = 0; i < scholarInfo.length; i++){//puting each status into res as a string array
+        res[i] = new Array(statusToString(statusArr[i]));
+    }
+    return res; 
 }
 
 // first go onto google app script and press play to print out the names of those who haven't done it
--- SlidesAPI.ts
+++ SlidesAPI.ts
@@ -3,22 +3,22 @@
   * elements in a sample presentation:
   * https://docs.google.com/presentation/d/1EAYk18WDjIG-zp_0vLm3CsfQh_i8eXc67Jo2O9C6Vuc/edit
   */
- const yearToSlides = new Map([
-    ["2020", "1gJOiAoghK9vZjXUspyzaUPHX1gPJIgLlFT4oXmQhN38"],
-    ["2019", "1b6O2rtCGCIL-qOMN8nGO_GFY9nsAUJlNnS9n1grPAb0"],
-    ["2018", "1NoJUD25u2qouG4QRT88gPJLJLRx6DkI5cCm4wL-wB8I"],
-    ["2017", "1kAO-Sc5gv4rl4fRlEq3JCPCSitdj1zpFCFi37fu4QRU"],
-    ["2016", "1PQfLl5pHbWSzSnxUhaQEyaOOvQizydEYagXlCiRkl0M"],
-    ["2015", "1csNUaSdueHI4Q9SHQnUkLza3Q_R0hX5k1x0SugJZVTE"],
+ const yearToSlides = new Map([ // use ints as the keys
+    [2020, "1gJOiAoghK9vZjXUspyzaUPHX1gPJIgLlFT4oXmQhN38"],
+    [2019, "1b6O2rtCGCIL-qOMN8nGO_GFY9nsAUJlNnS9n1grPAb0"],
+    [2018, "1NoJUD25u2qouG4QRT88gPJLJLRx6DkI5cCm4wL-wB8I"],
+    [2017, "1kAO-Sc5gv4rl4fRlEq3JCPCSitdj1zpFCFi37fu4QRU"],
+    [2016, "1PQfLl5pHbWSzSnxUhaQEyaOOvQizydEYagXlCiRkl0M"],
+    [2015, "1csNUaSdueHI4Q9SHQnUkLza3Q_R0hX5k1x0SugJZVTE"],
   ]);
 
-  const yearToStart = new Map([
-    ["2020", 2],
-    ["2019", 1],
-    ["2018", 1],
-    ["2017", 1],
-    ["2016", 1],
-    ["2015", 1],
+  const yearToStart = new Map([ // use ints as the keys
+    [2020, 2],
+    [2019, 1],
+    [2018, 1],
+    [2017, 1],
+    [2016, 1],
+    [2015, 1],
   ]);
 
   
@@ -28,10 +28,9 @@
     [TIMEOFYEAR.SPRING, /Spring Semester\s*([\w]+.*)+\s*Summer Term/]
   ])
   
- function checkIAP(cohortYear:string, name:string, toy: TIMEOFYEAR) : IAP_STATUS{
+ function checkIAP(cohortYear:number, name:string, timeOfYear: TIMEOFYEAR) : IAP_STATUS{
     var schoooYear = startOfSemester.year;// make this var according to whatever school year it is. eg. ig in SY 2020-2021 put 2020
 
-    let cohortYearInt = parseInt(cohortYear);
     let presentationId = yearToSlides.get(cohortYear);
     let startIdx = yearToStart.get(cohortYear);
     let presentation = SlidesApp.openById(presentationId);
@@ -44,13 +43,13 @@
     for(let i = startIdx; i < slides.length; i++){
       let shapes = slides[i].getShapes();// the important shape will hold the name of the scholar
       let groups = slides[i].getGroups();
-      let groupnum = (schoooYear-cohortYearInt < 3 ? schoooYear-cohortYearInt : 3);
+      let groupnum = (schoooYear-cohortYear < 3 ? schoooYear-cohortYear : 3);
       let cdrn = groups[groupnum].getChildren();// the four groups are respective to freshman, sophomore, junior, and senior years and hold the classes taken
-
       let slideName = shapes[3].getText().asString();// the shape at index 3 holds the scholar's name
+
       if(slideName.includes(name)){
         
-        let re = fallSpringRegex.get(toy)//use regex to get text between 'Fall Semester' and 'Winter Term' or to get text between 'Spring Semester' and 'Summer Term'
+        let re = fallSpringRegex.get(timeOfYear)//use regex to get text between 'Fall Semester' and 'Winter Term' or to get text between 'Spring Semester' and 'Summer Term'
         let info = cdrn[1].asShape().getText().asString();
         let match = re.exec(info); 
         
@@ -65,15 +64,35 @@
     }
     console.log("couldn't find scholar");
     return IAP_STATUS.INCOMPLETE; 
-    // let pes = slides[5].getShapes();//
+    /* let pes = slides[5].getShapes();//
     // let grs = slides[5].getGroups();// the four groups are respective to freshman, sophomore, junior, and senior years
     // let cdrn = grs[0].getChildren();// choosing freshman year with 0; sophomore 1; junior 2; senior 3
-    // console.log("pes len: " + pes.length + " groups len: " + grs.length);
-    // console.log("Name: " + pes[3].getText().asString());
-    // console.log("Group \n" + cdrn[0].asShape().getText().asString() + "\n" + cdrn[1].asShape().getText().asString() );//
+    */
+  }
+
+  function checkIAPBySlides(cohortYear:number, timeOfYear: TIMEOFYEAR, statusArr :IAP_STATUS[]){
+    var schoooYear = startOfSemester.year;// make this var according to whatever school year it is. eg. ig in SY 2020-2021 put 2020
+    let presentationId = yearToSlides.get(cohortYear);
+    let startIdx = yearToStart.get(cohortYear);
+    let presentation = SlidesApp.openById(presentationId);
+    let slides = presentation.getSlides();
+    Logger.log('The presentation contains %s slides:', slides.length);
+    console.log("slides len: " + slides.length);
+
+    for(let i = 0; i < slides.length; i++){//going through each slide
+      let shapes = slides[i].getShapes();// the important shape will hold the name of the scholar
+      let groups = slides[i].getGroups();
+      let groupnum = (schoooYear-cohortYear < 3 ? schoooYear-cohortYear : 3);
+      let cdrn = groups[groupnum].getChildren();// the four groups are respective to freshman, sophomore, junior, and senior years and hold the classes taken
+      let slideName = shapes[3].getText().asString();// the shape at index 3 holds the scholar's name
+      
+      for(let j = 0; j < scholarInfo.length; j++){//going through each student to find the student that matches the current slide
+        if(scholarInfo[j].cohort)
+      }
+    }
 
   }
 
   function main(){
-    checkIAP('2019', 'Joel Black Jr', TIMEOFYEAR.FALL);
+    checkIAP(2019, 'Joel Black Jr', TIMEOFYEAR.FALL);
   }
