1a50a447868d076113c2113ae01a20ae3cdb70b5
Travious46 <46880742+Travious46@users.noreply.github.com>
2021-03-30 16:52:28 -0400

    updating memo functions

--- MemoFunctions.ts
+++ MemoFunctions.ts
@@ -1,10 +1,12 @@
-function memo(): void{
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+"use strict";
+function memo():void {
     const memo = SpreadsheetApp.getActiveSpreadsheet().getSheets()[0];
-    var memoWahf = memo?.getRange("E7").getValue() as string;
+    const memoWahf = memo.getRange("E7").getValue()as string;
+    // var memoWPL = memo?.getRange("G8").getValue() as string;
+    // var formulas = getFormulaResult(`=COUNT(UNIQUE(Database!${memoWahf}4:${memoWahf}165))-2`);
+    // var results = memo?.getRange("G13").setValue(formulas);
 
-    var formulas = getFormulaResult(`=COUNT(UNIQUE(Database!${memoWahf}4:${memoWahf}165))-2`);
-    var results = memo?.getRange("G13").setValue(formulas);
-
-    // setCellFormula(memo, 13, 7, `=COUNT(UNIQUE(Database!${memoWahf}4:${memoWahf}165))-2`);
-
+    setCellFormula(memo, 13, 7, `=COUNT(UNIQUE(Database!${memoWahf}4:${memoWahf}165))-2`);
+    setCellFormula(memo, 13, 8, `=COUNTIF(Database!${memoWahf}4:${memoWahf}165,"Not Found")`);
 }
